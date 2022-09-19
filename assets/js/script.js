3d9137a867adb2558a101139c63134c58e23063a
Francis Targanski <francis@targanski.com>
2022-09-19 12:29:51 -0300

    responsividade slick

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,10 +172,34 @@ for (let empresa of Object.keys(marcas)) {
 
 //Carousel Slick - Marcas
 $(".carouselMarcasSlick").slick({
-  infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 1,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
+
+
 
 //Catálogo de Competições
 class Compet {
@@ -289,9 +313,31 @@ for (let compet of Object.keys(compets)) {
 
 //Carousel Slick - Competições
 $(".carouselCompets").slick({
-  infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 1,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 
 
